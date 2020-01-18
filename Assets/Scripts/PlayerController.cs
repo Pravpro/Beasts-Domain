9a4f54eb2bf1f323a01c89fa270a5a1e2e84a194
J-Xiao <xiaojijundavid@gmail.com>
2020-01-18 14:36:30 -0500

    Player collects and drop collectibles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     // Create variable for movement speed
     public float speed;
+    public GameObject collectible;
+    // private int count = 0;
+    // private List<Collider> colliders = new List<Collider>();
 
+    private void start()
+    {
+
+    }
 
     private void FixedUpdate()
     {
@@ -25,12 +32,42 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(new Vector3(-1, 0, 0) * speed);
         }
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Instantiate(collectible, new Vector3(0, 1, 0), Quaternion.identity);
+        }
     }
+
+    void OnTriggerStay(Collider col) 
+    {
+        if(col.tag == "Collectible" && Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("Stay collectible and E");
+            Destroy (col.gameObject);
+        }
+    }
+
+    // void OnTriggerEnter(Collider col)
+    // {
+    //     Debug.Log("Enter");
+    //     if(col.tag == "Collectible")
+    //     {
+    //         Debug.Log("Enter collectible");
+    //         // colliders.add(col);
+    //     }
+    // }
+    // void OnTriggerEnter(Collider col)
+    // {
+    //     Debug.Log("Collision");
+    //     if(col.tag == "Collectible")
+    //     {
+    //         Debug.Log("Exit collectible");
+    //         colliders.remove(col);
+    //     }
+    // }
 }
