c3e26858685bcfa1f9ed8fc319566dec4978934c
Kaori Tomihara <Kaori.Tomihara@mail.utoronto.ca>
2020-01-21 17:35:13 -0500

    added slingshot features

---
+++ Assets/Scripts/ThrowRock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowRock : MonoBehaviour
+{
+    public GameObject throwObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.S) )
+        {
+            Debug.Log("throwing the rock");
+            GameObject m_rock = Instantiate(throwObject, 
+                                            transform.position, 
+                                            transform.rotation) as GameObject;
+        }
+    }
+}
---
+++ Assets/Scripts/ThrowableController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableController : MonoBehaviour
+{
+    public float force = 500.0f;
+    public float up_velocity = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody  m_rid  = GetComponent<Rigidbody>();
+            
+        m_rid.useGravity = true;
+        m_rid.AddForce((transform.forward + Vector3.up * up_velocity) * force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name != "Player")
+        {
+            Debug.Log("collision detected, delete the object");
+            Destroy(gameObject);
+        }  
+    }
+}
