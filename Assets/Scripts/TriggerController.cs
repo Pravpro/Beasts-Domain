81eb51bc2b81a562353cb4d70387bcdb30235bb0
J-Xiao <xiaojijundavid@gmail.com>
2020-02-04 18:15:14 -0500

    Added trigger object and controller

---
+++ Assets/Scripts/TriggerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour
+{
+    public bool triggered = false;
+    private AIController script;
+    private GameObject monster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        monster = GameObject.FindGameObjectWithTag("Monster");
+        script = monster.GetComponent<AIController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "flyingRock" && !this.triggered)
+        {
+            this.triggered = true;
+            Vector3 scale = transform.localScale;
+            scale.y = 6;
+            transform.localScale = scale;
+            Vector3 pos = transform.position;
+            pos.y = 3;
+            transform.position = pos;
+        }
+        if (col.tag == "Monster" && this.triggered)
+        {
+            if (script.hp > 0)
+                script.hp -= 1;
+            Debug.Log("monster lose health to " + script.hp);
+            if (script.hp <= 0)
+                Debug.Log("Monster dies!");
+        }
+    }
+}
