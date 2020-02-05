03ae0d8dfdb0540e25b6f6b8459c428d274d28f3
J-Xiao <xiaojijundavid@gmail.com>
2020-02-04 19:11:05 -0500

    Trigger resets after 10 seconds

--- Assets/Scripts/TriggerController.cs
+++ Assets/Scripts/TriggerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class TriggerController : MonoBehaviour
 {
     public bool triggered = false;
+    public int resetTime = 500;
+    private int resetTimer = -1;
+    public float triggerCoeff = 3f;
     private AIController script;
     private GameObject monster;
     // Start is called before the first frame update
@@ -20,17 +23,46 @@ public class TriggerController : MonoBehaviour
         
     }
 
+    void FixedUpdate()
+    {
+        if (this.resetTimer >= 0)
+        {
+            this.resetTimer -= 1;
+        }
+        if (this.resetTimer == 0)
+        {
+            untrigger();
+        }
+    }
+
+    void trigger() 
+    {
+        this.resetTimer = resetTime;
+        this.triggered = true;
+        this.modifySize(this.triggerCoeff);
+    }
+
+    void untrigger()
+    {
+        this.triggered = false;
+        this.modifySize(1/this.triggerCoeff);
+    }
+
+    void modifySize(float coeff)
+    {
+        Vector3 pos = transform.position;
+        pos.y *= coeff;
+        transform.position = pos;
+        Vector3 scale = transform.localScale;
+        scale.y = pos.y * 2;
+        transform.localScale = scale;
+    }
+
     void OnTriggerEnter(Collider col)
     {
         if (col.name == "flyingRock" && !this.triggered)
         {
-            this.triggered = true;
-            Vector3 scale = transform.localScale;
-            scale.y = 6;
-            transform.localScale = scale;
-            Vector3 pos = transform.position;
-            pos.y = 3;
-            transform.position = pos;
+            this.trigger();
         }
         if (col.tag == "Monster" && this.triggered)
         {
