c51872550eede269610fd6b2fc8eadb1b23deba6
Pravpro <praviradlakha@hotmail.com>
2020-02-05 13:15:39 -0500

    Launch Arc and rock are synced with speed controls! (U and I)

--- Assets/Scripts/SlingshotController.cs
+++ Assets/Scripts/SlingshotController.cs
@@ -36,14 +36,10 @@ public class SlingshotController : MonoBehaviour
                                             player.transform.rotation) as GameObject;
             Rigidbody m_rb = m_rock.GetComponent<Rigidbody>();
 
-            //force = LaunchArcMesh.velocity * m_rb.mass * 35;
-            // m_rb.AddForce((player.transform.forward + Vector3.up) * force);
-            // param of AddForce Forcemode.impulse
-
+            // Add the launch arc forces to the throwable
             LaunchArcMesh arcScript = arc.GetComponent<LaunchArcMesh>();
             m_rb.AddForce(Quaternion.AngleAxis(arcScript.angle, arc.transform.right) * arc.transform.up * arcScript.velocity, ForceMode.Impulse);
-            //currentRigidbody.AddForce(Quaternion.AngleAxis((launchArcRenderer.angle % 180f) - 90, launchArcRenderer.transform.forward) * launchArcRenderer.transform.up * launchArcRenderer.velocity * throwMultiplier, ForceMode.Impulse);
-
+            
             // Destroy Arc when slingsot launched
             Destroy(arc);
         }
