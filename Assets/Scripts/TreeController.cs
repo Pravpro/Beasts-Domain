a67d25dc8e7ca5df1d003d54be1ea0828ba14110
Jijun X <xiaojijundavid@gmail.com>
2020-02-06 02:19:51 -0500

    Added tree controller script

---
+++ Assets/Scripts/TreeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Monster")
+        {
+            //Instantiate(log, new Vector3(transform.position.x, 0.5f, transform.position.z), transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
