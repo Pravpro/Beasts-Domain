aa13c9562e5355021867ebf3762bad7170296963
Jijun X <xiaojijundavid@gmail.com>
2020-02-06 05:25:39 -0500

    Reset after player fails.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -29,6 +30,7 @@ public class PlayerController : MonoBehaviour
     {
         if (hp <= 0)
         {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // "TitleScreen"
             //Debug.Log("Player hp below 0");
             return;
         }
