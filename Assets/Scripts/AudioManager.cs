2db32af8234d535d8c9a0dbe3585de579ba9bd86
Pravpro <praviradlakha@hotmail.com>
2020-03-03 20:05:44 -0500

    deleted levelBuildBackup

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Clips")]
+    public AudioClip landingClip;
+
+
+    [HideInInspector] public AudioSource landing;
+
+
+    void Awake()
+    {
+        landing = AddAudio(landingClip, false, false, 0.7f);
+    }
+
+    AudioSource AddAudio(AudioClip clip, bool loop, bool playAwake, float vol)
+    {
+        AudioSource newAudio = gameObject.AddComponent<AudioSource>();
+        newAudio.clip = clip;
+        newAudio.loop = loop;
+        newAudio.playOnAwake = playAwake;
+        newAudio.volume = vol;
+        return newAudio;
+    }
+
+    public void Play(AudioSource toPlay, bool randomPitch = false)
+    {
+        //if (toPlay == sourceWithMultipleClips)
+        //{
+        //    sourceWithMultipleClips.clip = clips[Random.Range(0, clips.Length)];
+        //}
+
+        if (randomPitch)
+        {
+            toPlay.pitch = Random.Range(0.8f, 1.2f);
+        }
+        toPlay.Play();
+        toPlay.pitch = 1f;
+    }
+}
