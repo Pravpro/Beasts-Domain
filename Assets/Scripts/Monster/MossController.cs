7cb5dafe9f3875f0fe4e4e8488d964cfbeff0942
Jijun X <xiaojijundavid@gmail.com>
2020-03-11 08:26:59 -0400

    Added moss balls that are thrown mid air, added script to create moss area. Added moss area prefab, added tag for it.

---
+++ Assets/Scripts/Monster/MossController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossController : MonoBehaviour
+{
+    public GameObject mossAreaObj;
+    public float mossDisappearTime = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag != "Monster" && col.tag != "Moss" && col.name != "Arena")
+        {
+            //Debug.Log("moss hit: " + col.name);
+            Vector3 pos =  transform.position;
+            pos.y = 0;
+            GameObject mossArea = Instantiate(mossAreaObj, pos, transform.rotation);
+            Destroy(mossArea, mossDisappearTime);
+            Destroy(gameObject);
+        }
+    }
+}
