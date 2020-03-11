2e58141e055666d6807cdb04df207703d8b8b9c1
Pravpro <praviradlakha@hotmail.com>
2020-03-10 21:36:19 -0400

    Working on Music triggers

---
+++ Assets/Scripts/Audio/MusicController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public GameObject player;
+    public AudioManagerMain audioManager;
+
+    private void Start()
+    {
+        audioManager.Play(audioManager.homebase);
+        audioManager.Play(audioManager.moodboard);
+        audioManager.Play(audioManager.strings);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Homebase")
+            audioManager.HomebaseVolume();
+        else if (other.name == "Training")
+            audioManager.MoodboardVolume();
+        else if (other.name == "Alley")
+            audioManager.BossVolume();
+        else if (other.name == "Arena")
+            audioManager.HomebaseVolume();
+
+    }
+}
