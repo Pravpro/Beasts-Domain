2416f8de192cdd37c5be28da13569c180401c2ad
Kaori Tomihara <kaori.tomihara@mail.utoronto.ca>
2020-03-14 04:54:47 -0400

    disable button prompt when slingshot

--- Assets/Scripts/ButtonPrompt.cs
+++ Assets/Scripts/ButtonPrompt.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class ButtonPrompt : MonoBehaviour
 {
-    private GameObject pushPrompt;
+    private GameObject m_buttonPromptCanvas;
+    private Player m_playerInput;
     // Start is called before the first frame update
     void Start()
     {
-        pushPrompt = GameObject.Find("push");
+        m_buttonPromptCanvas = GameObject.Find("ButtonPromptCanvas");
+        m_playerInput = ReInput.players.GetPlayer(0 /* player id */);
 
-
-        // set all the be false
-        pushPrompt.SetActive(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        // TODO: check for pause as well
+        
+        if (m_playerInput.GetButton("Aim"))
+            m_buttonPromptCanvas.SetActive(false);
+        else if (m_playerInput.GetButtonUp("Aim"))
+            m_buttonPromptCanvas.SetActive(true);
         
     }
 }
