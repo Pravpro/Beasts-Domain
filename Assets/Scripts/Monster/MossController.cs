594ee8e4eb43e8efa2c6922c3567d2d372ed4839
Jijun X <xiaojijundavid@gmail.com>
2020-03-19 14:13:06 -0400

    Hacky fix to make the moss area stay on top of ground

--- Assets/Scripts/Monster/MossController.cs
+++ Assets/Scripts/Monster/MossController.cs
@@ -20,11 +20,11 @@ public class MossController : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
-        if (col.tag != "Monster" && col.tag != "Moss" && col.name != "Arena")
+        if (col.name == "Wall_025x3x1 (4)")//(col.tag != "Monster" && col.tag != "Moss" && col.name != "Arena")
         {
             // Debug.Log("moss hit: " + col.name);
             Vector3 pos = transform.position;
-            pos.y = -0.25f;
+            pos.y = -0.1f;
             GameObject mossArea = Instantiate(mossAreaObj, pos, transform.rotation);
             Destroy(mossArea, mossDisappearTime);
             Destroy(gameObject);
