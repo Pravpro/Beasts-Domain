6aac0a872f7be1733bb61acc5171bc67c424c345
Pravpro <praviradlakha@hotmail.com>
2020-03-31 21:42:45 -0400

    Fire Light flickering script made/added

---
+++ Assets/Scripts/FlickerEffect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Used some code from https://gist.github.com/sinbad/4a9ded6b00cf6063c36a4837b15df969 for smoothing
+
+[RequireComponent(typeof(Light))]
+public class FlickerEffect : MonoBehaviour
+{
+    public float minIntensity, maxIntensity;
+    // Higher value means smoother flickering.
+    [Range(1,50)]
+    public int smoothing = 5;
+
+    // Use Queueing for averages
+    Queue<float> smoothQueue;
+    float lastSum;
+
+    Light pointLight;
+
+
+    void Start()
+    {
+        pointLight = GetComponent<Light>();
+        smoothQueue = new Queue<float>(smoothing);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(smoothQueue.Count >= smoothing)
+        {
+            lastSum -= smoothQueue.Dequeue();
+        }
+
+        float newVal = Random.Range(minIntensity, maxIntensity);
+        smoothQueue.Enqueue(newVal);
+        lastSum += newVal;
+        
+        // Use average for new intensity. Creates smoother intensity changes
+        pointLight.intensity = lastSum / (float)smoothQueue.Count;
+    }
+}
