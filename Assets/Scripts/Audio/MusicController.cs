5569d807dc9d75463cb3fcc24faffb958696a512
Pravpro <praviradlakha@hotmail.com>
2020-04-01 12:57:14 -0400

    Musc Controller added onto AudioManager for clean implementation

--- Assets/Scripts/Audio/MusicController.cs
+++ Assets/Scripts/Audio/MusicController.cs
@@ -5,45 +5,59 @@ using UnityEngine;
 public class MusicController : MonoBehaviour
 {
     public AudioManagerMain audioManager;
+    public GameObject musicTriggerer;
 
     private AudioManagerMain.SnapshotState pauseState, resumeState;
 
     private void Start()
-    {        
+    {
         audioManager.Play(audioManager.homebase);
         audioManager.Play(audioManager.moodboard);
         audioManager.Play(audioManager.strings);
         audioManager.Play(audioManager.birds);
+
+        /* Used to add this component onto the Music Area trigerrer (most likely the player).
+         * This will not go through once it's added onto the object since musicTriggere will be null 
+         */
+        if (musicTriggerer)
+        {
+            MusicController musicManager = musicTriggerer.AddComponent<MusicController>();
+            musicManager.audioManager = this.audioManager;
+            Destroy(this);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("The trigger is: " + other.name);
-        if (other.name == "Homebase")
+        SetMusic(other.name);
+    }
+
+    private void SetMusic(string trigger)
+    {
+        if (trigger == "Homebase")
         {
             audioManager.SetVolume(AudioManagerMain.SnapshotState.Homebase);
             pauseState = AudioManagerMain.SnapshotState.PauseHombase;
             resumeState = AudioManagerMain.SnapshotState.ResumeHomebase;
         }
-        else if (other.name == "Training")
+        else if (trigger == "Training")
         {
             audioManager.SetVolume(AudioManagerMain.SnapshotState.Training);
             pauseState = AudioManagerMain.SnapshotState.PauseTraining;
             resumeState = AudioManagerMain.SnapshotState.ResumeTraining;
         }
-        else if (other.name == "Alley")
+        else if (trigger == "Alley")
         {
             audioManager.SetVolume(AudioManagerMain.SnapshotState.Alleys);
             pauseState = AudioManagerMain.SnapshotState.Alleys;
             resumeState = AudioManagerMain.SnapshotState.Alleys;
         }
-        else if (other.name == "Arena")
+        else if (trigger == "Arena")
         {
             audioManager.SetVolume(AudioManagerMain.SnapshotState.Arena);
             pauseState = AudioManagerMain.SnapshotState.PauseArena;
             resumeState = AudioManagerMain.SnapshotState.ResumeArena;
         }
-
     }
 
     public AudioManagerMain.SnapshotState getPauseState()
