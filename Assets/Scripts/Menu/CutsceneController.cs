bd7dec94848168fb79e06ffcb463c82e418fe10e
Pravpro <praviradlakha@hotmail.com>
2020-04-19 20:45:54 -0400

    Added loadScene on cutscene stop. Added delay between loading next scene.

--- Assets/Scripts/Menu/CutsceneController.cs
+++ Assets/Scripts/Menu/CutsceneController.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.Video;
 using Rewired;
 
 public class CutsceneController : MonoBehaviour
 {
+    [Tooltip("How long into the cutscene skipping is allowed.")]
     public float skipDelay = 5f;
+    [Tooltip("The object that will show for skip option.")]
     public GameObject skipImage;
-    
+
+    private VideoPlayer cutscene;
+    private bool started, finished, skipped = false;
+
     // input from rewired
     private int m_playerID = 0;
     private Player m_playerInput;
@@ -17,13 +23,29 @@ public class CutsceneController : MonoBehaviour
     {
         skipImage.SetActive(false);
         m_playerInput = ReInput.players.GetPlayer(m_playerID);
+        cutscene = FindObjectOfType<VideoPlayer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (skipImage.activeSelf && m_playerInput.GetButtonDown("Submit")) SceneManager.LoadScene("TitleScreen");
+        if (!started && cutscene.isPlaying) started = true;
+        if (started && !cutscene.isPlaying) finished = true;
+        if (skipImage.activeSelf && m_playerInput.GetButtonDown("Submit")) skipped = true;
 
-        if (Time.time > skipDelay && !skipImage.activeSelf) skipImage.SetActive(true);
+        // Load title screen if skipped or if cutscene is done
+        if (skipped || finished) StartCoroutine(LoadNextScene());
+        // Wait till specified time to show skip image (which allows skip)
+        else if (Time.time > skipDelay && !skipImage.activeSelf) skipImage.SetActive(true);
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        skipImage.SetActive(false);
+        if (cutscene.isPlaying) cutscene.Stop();
+
+        yield return new WaitForSeconds(1.5f);
+
+        SceneManager.LoadScene("TitleScreen");
     }
 }
